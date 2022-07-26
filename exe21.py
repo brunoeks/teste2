bccc7f2447b5c553ac6a6fa45b904ee548a65bef
brunoeks <bruno.eks@hotmail.com>
2022-07-27 00:55:36 +0100

    audio

--- exe21.py
+++ exe21.py
@@ -1,5 +1,11 @@
 import pygame
+
+# Inicializando o mixer PyGame
+pygame.mixer.init()
+
+# Iniciando o Pygame
 pygame.init()
+
 pygame.mixer.music.load('Skyrim.mp3')
-pygame.mixer.music.play()
+pygame.mixer.music.play(loops=0, start=0.0)
 pygame.event.wait()
